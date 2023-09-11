dd3f430c5a28dcb8cd1a12348e19887c9e538896
Stoney <chisomd90@gmail.com>
2023-09-11 09:25:31 +0100

    API

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def get_info():
     utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
     # Get GitHub URLs
-    script_url = os.environ.get('SCRIPT_URL', 'https://github.com/HNG-BACKEND/main.py')
-    source_code_url = os.environ.get('SOURCE_CODE_URL', 'https://github.com/HNG-BACKEND')
+    script_url = os.environ.get('SCRIPT_URL', 'https://github.com/chisomd90/HNG-BACKEND/blob/main/main.py')
+    source_code_url = os.environ.get('SOURCE_CODE_URL', 'https://github.com/chisomd90/HNG-BACKEND')
 
     # Construct the response JSON
     response = {
-        'slack_name': slack_name,
+        'chisom_daniel': slack_name,
         'current_day': current_day,
         'utc_time': utc_time,
-        'track': track,
+        'backend': track,
         'github_file_url': script_url,
         'github_repo_url': source_code_url,
         'status_code': 200
